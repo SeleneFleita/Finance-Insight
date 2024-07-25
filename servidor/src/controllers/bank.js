f8642567dbaa01e8b2ded24a3f66aca5bfa06760
antpnella <antonella.beti@gmail.com>
2024-07-25 02:11:48 -0300

    se modificaron los controllers

--- servidor/src/controllers/bank.js
+++ servidor/src/controllers/bank.js
@@ -5,48 +5,49 @@ import {pool} from '../bd/basedata.js';
 
 
 export const registerBank = async (req, res) => {
-    const { razon_social, cuit, mail_bank, telefono, password_bank, country, province } = req.body;
+    const { razon_social, cuit, mail_bank, telefono, password_bank, country, province} = req.body;
     try {
         
         // Verificar si el banco ya existe en la base de datos
-        const rows = await pool.query('SELECT cuit, mail_bank FROM bank WHERE cuit = ? ', [cuit]);
-        if (rows.length > 0) {
+        const existeUser = await pool.query('SELECT cuit FROM bank WHERE cuit = ? ', [cuit]);
+        if (existeUser.length > 0) {
             return res.status(400).json({ message: "El usuario que desea registrar ya existe en nuestro sistema" });
         }else{
-// Encriptar la contraseña
-const hashedPassword = await bcrypt.hash(password_bank, 5);
-// Obtener id_country y id_province
-const countryRow = await pool.query('SELECT id_country FROM country WHERE nombre = ?', [country]);
-const province = await pool.query('SELECT id_province FROM country WHERE nombre = ?', [province]);
-const id_country = countryRow[0].id_country;
-const id_province = province[0].id_province;
-//conseguir id provincia y pais(tabla)
-//const id_counyprov = await pool.query('SELECT id_counyprov FROM country_province WHERE id_country = ? AND id_province = ?', [id_country, id_province]);
-// Consulta de inserción
-const sql = 'INSERT INTO bank (razon_social, cuit, mail_bank, telefono, password_bank, id_country) VALUES (?, ?, ?, ?, ?, ?)';
-// Insertar el nuevo banco en la base de datos
-const result = await pool.query(sql, [razon_social, cuit, mail_bank, telefono, hashedPassword, id_country]);
-// Crear el token JWT
-const token = jwt.sign({ id: result.id_bank, role: 'bank' }, 'my_secret', { expiresIn: '1h' });
-// Respuesta después de la creación exitosa del banco
-res.status(201).json({
-    status: 201,
-    message: 'Usuario banco creado correctamente',
-    token,
-    Nuevo_usuario: {
-        razon_social,
-        cuit,
-        mail_bank,
-        telefono,
-        //id_counyprov
-    }
-});
+    // Encriptar la contraseña
+        const hashedPassword = await bcrypt.hash(password_bank, 5);
+    // Obtener id_country y id_province
+        
+
+        
+        
+        // Consulta de inserción
+        const sql = 'INSERT INTO bank (razon_social, cuit, mail_bank, telefono_bank, id_counyprov,  password_bank ) VALUES (?, ?, ?, ?, ?, ?)';
+        
+        const [pc] = await pool.query('SELECT cp.id_counyprov FROM country_province cp JOIN province p ON cp.id_province = p.id_province JOIN country c ON cp.id_country = c.id_country WHERE p.nombre = ? AND c.nombre = ?; ', [province, country])
+        const id_counyprov = pc[0].id_counyprov
+        // Insertar el nuevo banco en la base de datos
+        const result = await pool.query(sql, [razon_social, cuit, mail_bank, telefono,id_counyprov, hashedPassword]);
+        // Crear el token JWT
+        const token = jwt.sign({ id: result.id_bank, role: 'bank' }, 'my_secret', { expiresIn: '1h' });
+    // Respuesta después de la creación exitosa del banco
+    res.status(201).json({
+        status: 201,
+        message: 'Usuario banco creado correctamente',
+        token,
+        Nuevo_usuario: {
+            razon_social,
+            cuit,
+            mail_bank,
+            telefono,
+            id_counyprov
+        }
+    });
         }
         
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({ message: 'Error interno del servidor' });
-    }
+        } catch (error) {
+            console.log(error);
+            res.status(500).json({ message: 'Error interno del servidor' });
+        }
 };
 
 
